83cc9526702452a6e29bb4c486e4dbd345b6e89a
lin <lin@qq.com>
2024-11-26 19:13:21 +0800

    点位管理

---
+++ dkd-manage/src/main/java/com/dkd/manage/domain/vo/NodeVo.java
@@ -0,0 +1,21 @@
+package com.dkd.manage.domain.vo;
+
+import com.dkd.manage.domain.Node;
+import com.dkd.manage.domain.Partner;
+import com.dkd.manage.domain.Region;
+import lombok.Data;
+
+/**
+ * @desription
+ */
+@Data
+public class NodeVo extends Node {
+    //设备数量
+    private Integer vmCount;
+
+    //区域
+    private Region region;
+
+    //合作商
+    private Partner partner;
+}
